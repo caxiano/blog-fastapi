bc1a7b4dd30cdb25a0ca7d513fc17e76275211e4
Cassiano Shigueyuki Nishikawa <csnishikawa@gmail.com>
2026-01-09 23:21:45 -0300

    docs: changing config.py to production

--- src/config.py
+++ src/config.py
@@ -4,7 +4,7 @@ from pydantic_settings import BaseSettings, SettingsConfigDict
 class Settings(BaseSettings):
     model_config = SettingsConfigDict(env_file=".env", extra="ignore", env_file_encoding="utf-8")
     database_url: str = "sqlite:///../blog.db"
-    environment: str = "development"
+    environment: str = "production"
 
 
 settings = Settings()
