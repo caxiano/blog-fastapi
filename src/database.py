e7402953f88a190adebdd8d6cba64d0b9cce80aa
Cassiano Shigueyuki Nishikawa <csnishikawa@gmail.com>
2026-01-11 11:22:31 -0300

    fix: database connection on render

--- src/database.py
+++ src/database.py
@@ -4,14 +4,20 @@ import sqlalchemy as sa
 from src.config import settings
 
 
+final_db_url = settings.database_url
 database_args = {}
+
 if settings.environment == "production" and settings.database_url.startswith("postgresql"):
     database_args["ssl"] = True
+    if settings.database_url.startswith("postgresql://"):
+        final_db_url = settings.database_url.replace("postgresql://", "postgresql+asyncpg://", 1)
 
-database = databases.Database(settings.database_url, **database_args)
+
+database = databases.Database(final_db_url, **database_args)
 metadata = sa.MetaData()
 
 if settings.environment == "production":
-    engine = sa.create_engine(settings.database_url)
+    engine = sa.create_engine(final_db_url)
 else:
+    # Use the original URL for sqlite, which doesn't need the scheme replacement
     engine = sa.create_engine(settings.database_url, connect_args={"check_same_thread": False})
